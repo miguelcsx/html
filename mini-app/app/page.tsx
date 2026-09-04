1fd9032cc8839cb3e1915173fd02ff1e787a287f
Miguel Cárdenas <miguelecsx@gmail.com>
2026-09-03 19:14:39 -0500

    feat(app): add home page

---
+++ mini-app/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <section>
+      <h1 className="text-2xl font-bold">Mini App React</h1>
+      <p className="mt-2">
+        Proyecto que consume la API de paises usando useState y useEffect.
+      </p>
+      <p className="mt-2">Integrantes: Miguel Cardenas</p>
+    </section>
+  );
+}
